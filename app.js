7ffc2e6f2c240c295835e70fdf0b25dbcfa3d0a8
Allanmendez0 <kenfackmendez@gmail.com>
2024-10-24 14:53:21 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemManager from './ItemManager';
+
+const App = () => {
+  return (
+    <div>
+      <ItemManager />
+    </div>
+  );
+};
+
+export default App;
